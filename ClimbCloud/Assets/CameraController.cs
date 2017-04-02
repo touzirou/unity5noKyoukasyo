fba0471b55c4273ce4523511eae42d234d7b2c2c
tsujimochi <touzirou1908@gmail.com>
2017-04-03 00:28:25 +0900

    ネコジャンププロト

---
+++ ClimbCloud/Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * カメラを動かすよ
+ * @author tsujimochi
+ */
+public class CameraController : MonoBehaviour {
+
+    GameObject player;
+
+	void Start () {
+        this.player = GameObject.Find("cat");
+	}
+	
+	void Update () {
+        Vector3 playerPos = this.player.transform.position;
+        transform.position = new Vector3(transform.position.x, playerPos.y, transform.position.z);
+	}
+}
---
+++ ClimbCloud/Assets/ClearDirector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // LoadScene を使うために必要!!
+
+/**
+ * シーンを繊維させるよ
+ * @author tsujimochi
+ */
+public class ClearDirector : MonoBehaviour {
+
+	void Start () {	
+	}
+
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+	}
+}
