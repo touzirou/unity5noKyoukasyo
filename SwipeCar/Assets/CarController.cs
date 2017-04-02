45b44cd1cab2401547dc57130cb7fdf9739b31b2
tsujimochi <touzirou1908@gmail.com>
2017-04-02 17:44:39 +0900

    車動かす

---
+++ SwipeCar/Assets/CarController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * 車を動かすよ
+ * @author tsujimochi
+ */
+public class CarController : MonoBehaviour
+{
+
+    float speed = 0;
+    Vector2 startPos;
+
+    void Start()
+    {
+        // nothing
+    }
+
+    void Update()
+    {
+        // マウスがクリックされたら初速度を設定
+        //if(Input.GetMouseButtonDown(0))
+        //{
+        //    this.speed = 0.2f;
+        //}
+
+        // スワイプの長さを求める
+        if (Input.GetMouseButtonDown(0))
+        {
+            // マウスをクリックした座標
+            this.startPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            // マウスを離した座標
+            Vector2 endPost = Input.mousePosition;
+            float swipeLength = (endPost.x - this.startPos.x);
+
+            // スワイプの長さを初速度に変換する
+            this.speed = swipeLength / 1000.0f;
+
+            // 効果音再生
+            GetComponent<AudioSource>().Play();
+        }
+
+        transform.Translate(this.speed, 0, 0); // 移動
+        this.speed = this.speed * 0.98f;  // 減速
+    }
+}
---
+++ SwipeCar/Assets/GameDirector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // UI部品を使うために必要！
+
+/**
+ * 距離情報を表示するよ
+ * @author tsujimochi
+ */
+public class GameDirector : MonoBehaviour
+{
+
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+
+    void Start()
+    {
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+    }
+
+    void Update()
+    {
+        // 車と旗までの距離を計算
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+
+        if(length >= 0)
+        {
+            this.distance.GetComponent<Text>().text = "ゴールまで" + length.ToString("F2") + "m";
+        }else
+        {
+            this.distance.GetComponent<Text>().text = "ゲームオーバー";
+        }
+
+    }
+}
