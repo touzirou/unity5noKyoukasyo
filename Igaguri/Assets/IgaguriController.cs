b180c8eea8ea39e21a840c2fedf8ea0b3502ff9e
tsujimochi <touzirou1908@gmail.com>
2017-04-03 23:36:19 +0900

    イガグリ投げる

---
+++ Igaguri/Assets/IgaguriController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * イガグリ飛ばしますか？
+ * @author tsujimochi
+ */
+public class IgaguriController : MonoBehaviour
+{
+
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+
+
+    void Start()
+    {
+        // Shoot(new Vector3(0, 200, 2000));
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Igaguri/Assets/IgaguriGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * イガグリ工場
+ * @author tsujimochi
+ */
+public class IgaguriGenerator : MonoBehaviour {
+
+    public GameObject igaguriPrefab;
+
+	void Start () {
+		// nothing
+	}
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject igaguri = Instantiate(igaguriPrefab) as GameObject;
+            // igaguri.GetComponent<IgaguriController>().Shoot(new Vector3(0, 200, 2000));
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            igaguri.GetComponent<IgaguriController>().Shoot(worldDir.normalized * 2000);
+        }
+	}
+}
