82a72757726ac51e51fd7fc4c1fe9ae906050ef1
tsujimochi <touzirou1908@gmail.com>
2017-04-04 23:55:30 +0900

    リンゴ集め

---
+++ AppleCatch/Assets/BasketController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * バスケット動かす
+ * @author tsujimochi
+ */
+public class BasketController : MonoBehaviour
+{
+    public AudioClip appleSE;
+    public AudioClip bombSE;
+    AudioSource aud;
+    GameObject director;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Debug.Log("キャッチ！");
+        switch (other.gameObject.tag)
+        {
+            case "Apple":
+                // Debug.Log("Tag=Apple");
+                this.aud.PlayOneShot(this.appleSE);
+                this.director.GetComponent<GameDirector>().GetApple();
+                break;
+            case "Bomb":
+                // Debug.Log("Tag=Bomb");
+                this.aud.PlayOneShot(this.bombSE);
+                this.director.GetComponent<GameDirector>().GetBomb();
+                break;
+            default:
+                Debug.Log("Unknown");
+                break;
+        }
+        Destroy(other.gameObject);
+    }
+
+
+    void Start()
+    {
+        this.aud = GetComponent<AudioSource>();
+        this.director = GameObject.Find("GameDirector");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                float x = Mathf.RoundToInt(hit.point.x);
+                float z = Mathf.RoundToInt(hit.point.z);
+                transform.position = new Vector3(x, 0.0f, z);
+            }
+        }
+    }
+}
---
+++ AppleCatch/Assets/ItemController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * アイテム落ちるよ
+ * @author tsujimochi
+ */
+public class ItemController : MonoBehaviour
+{
+
+    public float dropSpeed = -0.03f;
+
+    void Start()
+    {
+        // nothing
+    }
+
+    void Update()
+    {
+        transform.Translate(0, this.dropSpeed, 0);
+        if (transform.position.y < -0.1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
